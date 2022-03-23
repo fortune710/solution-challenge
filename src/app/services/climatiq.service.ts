9793c9e436e1b6694991d476d2fc71b1260281ce
fortune710 <fortune0208@yahoo.com>
2022-03-23 09:23:34 +0100

    Climatiq Api Service using Capacitor/Http

--- src/app/services/climatiq.service.ts
+++ src/app/services/climatiq.service.ts
@@ -11,17 +11,50 @@ export class ClimatiqService {
 
   constructor() { }
 
-  async getCarbonEstimateFromVehicle(distance:number, unit:string){
+  async getCarbonEstimateFromVehicle(distance:number, unit:string, passengers:number=1){
     const options:HttpOptions = {
       url: this.url+'/estimate',
       headers: this.httpHeader,
       data: {
         parameters: JSON.stringify({
+          passengers: passengers,
           distance: distance,
           distance_unit: unit
+          //Unit of distance. One of km, mi, nmi, m or ft. Default km
         })
       }
     }
     await Http.post(options)
+  }
+
+  async getCarbonEstimateFromFuel(volume:number, unit:string){
+    const options:HttpOptions = {
+      url: this.url+'/estimate',
+      headers: this.httpHeader,
+      data: {
+        parameters: JSON.stringify({
+          volume: volume,
+          volume_unit: unit
+          //Unit of volume. One of l, ml, m3, standard_cubic_foot or gallons_us
+        })
+      }
+    }
+    await Http.post(options)
+  }
+
+  async getCarbonEstimateFromEnergyConsumption(energy:number, unit:string="kWh"){
+    const options:HttpOptions = {
+      url: this.url+'/estimate',
+      headers: this.httpHeader,
+      data: {
+        parameters: JSON.stringify({
+          energy: energy,
+          energy_unit: unit
+          //Unit of energy. One of kWh, TJ, GJ or MMBTU
+        })
+      }
+    }
+    await Http.post(options)
+
   }
 }
