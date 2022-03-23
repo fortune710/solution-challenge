da4849645dabf771da99f62ffd05982206ace43f
fortune710 <fortune0208@yahoo.com>
2022-03-23 09:22:32 +0100

    Added Firebase Remote config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'solution-challenge-b441b',
+    appId: '1:776031906850:web:ae4e1c950974c014832e7b',
+    storageBucket: 'solution-challenge-b441b.appspot.com',
+    apiKey: 'AIzaSyCA2tp1yvoJQGG8_45QmFDVaMXOUarUB64',
+    authDomain: 'solution-challenge-b441b.firebaseapp.com',
+    messagingSenderId: '776031906850',
+    measurementId: 'G-MKKMRJL0RN',
+  },
   production: false
 };
 
