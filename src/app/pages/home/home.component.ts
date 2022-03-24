15cb0dfac290faf948cbf9a68484c00cd70fb83a
fortune710 <fortune0208@yahoo.com>
2022-03-24 15:44:43 +0100

    Added basics to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
+import { User } from "@angular/fire/auth";
+import { Platform } from '@ionic/angular'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  message: string | any;
+  data: any;
+  user: User | any = this.firebaseService.auth.currentUser
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService,
+              private platform: Platform) { 
+                this.data = localStorage.getItem('data')
+                //console.log(this.data)
+              }
 
   ngOnInit(): void {
+    console.log(this.user?.uid)
+    this.firebaseService.getUserDocument('users', this.user?.uid)
+    .then((response)=>{
+      this.data = response
+    })
+
+
+    if(this.platform.is('mobileweb')){
+      localStorage.setItem('data', this.data)
+    }
   }
 
 }
