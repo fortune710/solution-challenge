23657aa548f4e327e937aab48ce49ea80d22d35d
claire-ndupu <maryndupu.1@gmail.com>
2022-03-25 11:44:30 +0100

    Added log in page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UserDetails } from 'src/app/interfaces/schema';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+  displayName: string = '';
+  gender: string = '';
 
-  constructor() { }
+  data:UserDetails = {
+    displayName: this.displayName,
+    carbonBudgetForMonth: 0,
+    carbonHistory: [{
+      month: new Date().getMonth() + 1,
+      year: new Date().getFullYear() + 1,
+      carbonAmount: 0
+    }],
+    totalCarbonThisMonth: {
+      month: new Date().getMonth() + 1,
+      year: new Date().getFullYear() + 1,
+      carbonAmount: 0
+    }
+  };
+
+  logInForm: FormGroup | any
+  
+  getGender(event: any){
+    this.gender = event.target.value;
+  }
+
+  logIn(){
+    console.log(this.logInForm.value)
+    
+    this.firebaseService.signInWithEmail(this.logInForm.value.password, this.logInForm.value.email)
+    .then((details)=>{
+      this.data.displayName = this.logInForm.value.displayName
+      this.firebaseService.createUserDocument('users',details?.uid, this.data)
+    })
+  }
+
+  constructor(private firebaseService: FirebaseService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.logInForm = this.formBuilder.group({
+      displayName: [''],
+      email: [''],
+      password: [''],
+    })
   }
 
 }
+
+
