4a39ae49666e666bb79fb505318bb499c29c1316
fortune710 <fortune0208@yahoo.com>
2022-03-28 14:01:20 +0100

    finished the progress bar

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -1,14 +1,53 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CarbonAmountForUser } from 'src/app/interfaces/schema';
+import { CarbonAmountForUser, UserDetails } from 'src/app/interfaces/schema';
+
 @Component({
   selector: 'progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent implements OnInit {
-  @Input('data') data:CarbonAmountForUser | any;
-  constructor() { }
 
+  data: UserDetails = {
+    displayName: 'Fortune Alebiosu',
+    totalCarbonThisMonth: {
+      month: 2,
+      year: 2022,
+      carbonAmount: 250
+    },
+    carbonBudgetForMonth: 625,
+    carbonHistory:[
+      {
+        month: 2,
+        year: 2022,
+        carbonAmount: 250
+      },
+      {
+        month: 1,
+        year: 2022,
+        carbonAmount: 350
+      },
+      {
+        month: 12,
+        year: 2021,
+        carbonAmount: 324
+      },
+      {
+        month: 11,
+        year: 2021,
+        carbonAmount: 340
+      },
+      {
+        month: 10,
+        year: 2021,
+        carbonAmount: 297
+      },
+    ]
+  };
+  percent:number = (this.data.totalCarbonThisMonth.carbonAmount/this.data.carbonBudgetForMonth)*100;
+  
+  constructor() { }
+  
   ngOnInit(): void {
   }
 
