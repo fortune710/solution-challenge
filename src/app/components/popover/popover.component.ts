a3c6e1db21fbb307b903c4a796a64cd754272422
fortune710 <fortune0208@yahoo.com>
2022-03-28 14:01:06 +0100

    Finished the popover tracking carbon consumption

--- src/app/components/popover/popover.component.ts
+++ src/app/components/popover/popover.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popover.component.scss']
 })
 export class PopoverComponent implements OnInit {
+  emissionType: string|any;
 
+  getEmissionType(event:any){
+    this.emissionType = event.target.value;
+  }
   constructor() { }
 
   ngOnInit(): void {
