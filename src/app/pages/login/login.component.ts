28f701f37573a2d6a7c6ee95ce5aff2b3b361554
fortune710 <fortune0208@yahoo.com>
2022-03-29 16:24:04 +0100

    finished the login page with routing

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,8 @@ import { User } from '@angular/fire/auth';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UserDetails } from 'src/app/interfaces/schema';
+import { Router } from '@angular/router';
+import { FunctionalitiesService } from 'src/app/services/functionalities.service';
 
 @Component({
   selector: 'app-login',
@@ -28,10 +30,26 @@ export class LoginComponent implements OnInit {
     .then((details)=>{
       this.firebaseService.getUserDocument('users',details?.uid)
     })
+    .then(()=>{
+      this.router.navigate(['/home'])
+    })  
+    .catch((err)=>{
+      let errCode = err.code;
+      switch(errCode){
+        case 'auth/wrong-password':
+          this.uiService.createToast("Password does not match the account");
+          break;
+        default:
+          this.uiService.createToast("There was an error while logging in");
+          break;
+      }
+    })
   }
 
   constructor(private firebaseService: FirebaseService,
-    private formBuilder: FormBuilder) { }
+              private formBuilder: FormBuilder,
+              private router:Router,
+              private uiService:FunctionalitiesService) { }
 
   ngOnInit(): void {
     this.logInForm = this.formBuilder.group({
