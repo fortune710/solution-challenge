4a633f3bf67cfae9c89c2408a6467787d3d1a456
fortune710 <fortune0208@yahoo.com>
2022-03-30 16:25:15 +0100

    refactored the fmethds in the class

--- src/app/services/climatiq.service.ts
+++ src/app/services/climatiq.service.ts
@@ -12,53 +12,53 @@ export class ClimatiqService {
 
   constructor() { }
 
-  async getCarbonEstimateFromVehicle(distance:number, unit:'km'|'mi'|'nmi'|'m'|'ft'="km", passengers:number=1, vehicle:string, fuel_source:string, engine_size:string, vehicle_age:string, vehicle_weight:string){
+  async getCarbonEstimateFromVehicle(distance:number, unit:'km'|'mi'|'nmi'|'m'|'ft'="km", passengers:number=1, vehicle:string='na', fuel_source:string='na', engine_size:string='na', vehicle_age:string='na', vehicle_weight:string='na'){
     const options:HttpOptions = {
       url: this.url+'/estimate',
       headers: this.httpHeader,
       data: {
         emission_factor: `passenger_vehicle-vehicle_type_${vehicle}-fuel_source_${fuel_source}-engine_size_${engine_size}-vehicle_age_${vehicle_age}-vehicle_weight_${vehicle_weight}`,
-        parameters: JSON.stringify({
+        parameters: {
           passengers: passengers,
           distance: distance,
           distance_unit: unit
           //Unit of distance. One of km, mi, nmi, m or ft. Default km
-        })
+        }
       }
     }
-    await Http.post(options)
+    return await Http.post(options)
   }
 
-  async getCarbonEstimateFromFuel(volume:number, unit:'l'|'ml'|'m3'|'standard_cubic_foot'|'gallons_us', vehicle:string, fuel_source:string, engine_size:string, vehicle_age:string, vehicle_weight:string){
+  async getCarbonEstimateFromFuel(volume:number, unit:'l'|'ml'|'m3'|'standard_cubic_foot'|'gallons_us'){
     const options:HttpOptions = {
       url: this.url+'/estimate',
       headers: this.httpHeader,
       data: {
-        emission_factor: `passenger_vehicle-vehicle_type_${vehicle}-fuel_source_${fuel_source}-engine_size_${engine_size}-vehicle_age_${vehicle_age}-vehicle_weight_${vehicle_weight}`,
-        parameters: JSON.stringify({
+        emission_factor: "fuel_type_petrol-fuel_use_na",
+        parameters: {
           volume: volume,
           volume_unit: unit
           //Unit of volume. One of l, ml, m3, standard_cubic_foot or gallons_us
-        })
+        }
       }
     }
-    await Http.post(options)
+    return await Http.post(options)
   }
 
-  async getCarbonEstimateFromEnergyConsumption(energy:number, unit:'kWh'|'TJ'|'GJ'|'MMBTU'="kWh", vehicle:string, fuel_source:string, engine_size:string, vehicle_age:string, vehicle_weight:string){
+  async getCarbonEstimateFromEnergyConsumption(energy:number, unit:'kWh'|'TJ'|'GJ'|'MMBTU'="kWh"){
     const options:HttpOptions = {
       url: this.url+'/estimate',
       headers: this.httpHeader,
       data: {
-        emission_factor: `passenger_vehicle-vehicle_type_${vehicle}-fuel_source_${fuel_source}-engine_size_${engine_size}-vehicle_age_${vehicle_age}-vehicle_weight_${vehicle_weight}`,
-        parameters: JSON.stringify({
+        emission_factor: `electricity-energy_source_grid_mix`,
+        parameters:{
           energy: energy,
           energy_unit: unit
           //Unit of energy. One of kWh, TJ, GJ or MMBTU
-        })
+        }
       }
     }
-    await Http.post(options)
+    return await Http.post(options)
 
   }
 
@@ -73,14 +73,14 @@ export class ClimatiqService {
     const opts:HttpOptions = {
       url:`https://beta3.api.climatiq.io/compute/${options.cloudProvider}/cpu`,
       headers: this.httpHeader,
-      data: JSON.stringify({
+      data: {
         region: options.region,
         cpu_count: options.coreCount,
         duration: options.duration,
         cpu_load: average_cpu_load,
         duration_unit: options.timeUnit,
         data_unit: options.dataUnit
-      })
+      }
     }
 
     return await Http.post(opts)
@@ -90,14 +90,14 @@ export class ClimatiqService {
     const opts:HttpOptions = {
       url:`https://beta3.api.climatiq.io/compute/${options.cloudProvider}/storage`,
       headers: this.httpHeader,
-      data: JSON.stringify({
+      data: {
         region: options.region,
         data_unit: options.dataUnit,
         duration: options.duration,
         duration_unit: options.timeUnit,
         data: options.dataAmount,
         storage_type: options.storageType
-      })
+      }
     }
     return await Http.post(opts)
   }
@@ -106,13 +106,13 @@ export class ClimatiqService {
     const opts:HttpOptions = {
       url:`https://beta3.api.climatiq.io/compute/${options.cloudProvider}/memory`,
       headers: this.httpHeader,
-      data: JSON.stringify({
+      data:{
         region: options.region,
         data_unit: options.dataUnit,
         duration: options.duration,
         duration_unit: options.timeUnit,
         data: options.dataAmount,
-      })
+      }
     }
     return await Http.post(opts)
   }
